98676744a4447ba64347e83e7db292025610d665
Daniel Fernando Palma Lopez <palma@Daniels-MacBook-Pro.local>
2017-07-06 09:32:14 +0200

    first iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,21 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+//app.set('layout','layout');
+
+app.get('/random', (req, res) => {
+  client.getRandomJoke().then((response) => {
+    //variable como objeto
+    let obj = {
+      joke: response.value,
+    };
+    res.render('index', obj);
+  });
+});
+
+
+app.listen(3050, () => {
+  console.log('Connected to the server!');
+});
